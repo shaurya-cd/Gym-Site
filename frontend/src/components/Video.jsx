265e5a1bccc33e4d798f73f49c2ca47619c5296e
shaurya <2447shaurya48@gmail.com>
2025-06-22 00:34:24 +0530

    added sorrect video

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import video from '../assets/gymvideo.mp4'
 
 function Video() {
   return (
     <div className='video' id='home'>
-
+      <video src={video} autoPlay loop muted></video>
     </div>
   )
 }
