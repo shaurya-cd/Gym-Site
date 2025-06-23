ee4f7509175e9ce0779447bc1b0e425514013320
shaurya <2447shaurya48@gmail.com>
2025-06-23 09:40:26 +0530

    changed service images url

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -2,43 +2,50 @@ import React from 'react'
 import ServiceCard from './Service-card'
 import axios from 'axios'
 import { useEffect,useState } from 'react'
+import service1 from '../assets/service1.jpg'
+import service2 from '../assets/service2.jpg'
+import service3 from '../assets/service3.jpg'
+import service4 from '../assets/service4.jpg'
+import service5 from '../assets/service5.png'
+import service6 from '../assets/service6.png'
+
 
 function Services() {
 
   const services = [
         {
             id:1,
-            imageURL: "../src/assets/service1.jpg",
+            imageURL: service1,
             title:"Personal Training",
             description:"Work one-on-one with certified fitness trainers who create custom workout plans tailored to your goals, fitness level, and lifestyle."
         },
         {
             id:2,
-            imageURL:"../src/assets/service2.jpg",
+            imageURL: service2,
             title:"Group Fitness Classes",
             description:"From Zumba to HIIT and Yoga, our dynamic group classes offer fun, motivation, and variety."
         },
         {
             id:3,
-            imageURL:"../src/assets/service3.jpg",
+            imageURL: service3,
             title:"Cardio & Strength Equipment",
             description:"Access top-tier machines and free weights in our fully equipped training zones, designed for all levels of cardio and resistance workouts."
         },
         {
             id:4,
-            imageURL:"../src/assets/service4.jpg",
+            imageURL: service4,
             title:"Nutrition Guidance",
             description:"Receive personalized diet plans and meal advice from our in-house nutrition experts to fuel your fitness journey and boost results."
         },
         {
             id:5,
-            imageURL:"../src/assets/service5.png",
+            imageURL: service5,
             title:"Fitness Assessment",
             description:"Track your progress with regular body composition analysis and fitness evaluations to keep your goals measurable and achievable."
         },
         {
             id:6,
-            imageURL:"../src/assets/service6.png",
+            imageURL: service6,
             title:"Locker Room & Amenities",
             description:"Enjoy clean, secure locker rooms with showers, towel service, and relaxation areas including steam or sauna (where available)."
         }
