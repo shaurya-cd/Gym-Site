1d4722612cab81afa5a00cb7755b8d111080a43f
shaurya <2447shaurya48@gmail.com>
2025-06-23 11:20:57 +0530

    padding correction

--- frontend/src/components/Plan_card.jsx
+++ frontend/src/components/Plan_card.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 function PlanCard() {
   return (
-    <div className='plancard-container'>
-        <div className="s-plan-cards">
-            <h1>clubpass</h1>
-            <h1 className='plan1'>Basic</h1>
-            <p>Essential access for beginners to kickstart their fitness journey</p>
-        </div>
-        <div className="s-plan-cards">
-            <h1>clubpass</h1>
-            <h1 className='plan2'>Standard</h1>
-            <p>Balanced workouts and group classes for consistent progress</p>
-        </div>
-        <div className="s-plan-cards">
-            <h1>clubpass</h1>
-            <h1 className='plan3'>Premium</h1>
-            <p>All-inclusive training, classes, and wellness — for serious results</p>
-        </div>
+    <div className="plancard">
+      <div className='plancard-container'>
+          <div className="s-plan-cards">
+              <h1>clubpass</h1>
+              <h1 className='plan1'>Basic</h1>
+              <p>Essential access for beginners to kickstart their fitness journey</p>
+          </div>
+          <div className="s-plan-cards">
+              <h1>clubpass</h1>
+              <h1 className='plan2'>Standard</h1>
+              <p>Balanced workouts and group classes for consistent progress</p>
+          </div>
+          <div className="s-plan-cards">
+              <h1>clubpass</h1>
+              <h1 className='plan3'>Premium</h1>
+              <p>All-inclusive training, classes, and wellness — for serious results</p>
+          </div>
+      </div>
     </div>
   )
 }
